ed893e5fcd3e259959f98e32bf9c2203a1b28b32
lin-dongyizhi7 <2985956026@qq.com>
2025-02-24 02:56:36 +0800

    wip: TRPC, upstash

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,18 @@
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 export default function Home() {
+  void trpc.hello.prefetch({ text: "YYS" });
+
   return (
-    <div>
-      I will load video in the future!
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
