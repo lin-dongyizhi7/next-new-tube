1900be8ebad948f54d83b1f5e3718fad1fc22619
lin-dongyizhi7 <2985956026@qq.com>
2025-02-24 17:10:10 +0800

    wip: video categories

--- src/app/(home)/protected/something/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-    return (
-        <div>
-            Only logged in users should see this subRoute.
-        </div>
-    )
-}
-
-export default Page;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,34 @@
-import { pgTable, text, timestamp, uniqueIndex, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  clerkId: text("clerk_id").unique().notNull(),
-  name: varchar({ length: 255 }).notNull(),
-  // TODO: add banner fields
-  imageUrl: text("image_url").notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().notNull(),
-}, (t)=> [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: text("clerk_id").unique().notNull(),
+    name: varchar({ length: 255 }).notNull(),
+    // TODO: add banner fields
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
+);
 
+export const categories = pgTable(
+  "categories",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar({ length: 255 }).notNull().unique(),
+    description: text("description"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("name_idx").on(t.name)]
+);
